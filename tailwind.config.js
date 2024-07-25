2cb9557ecbe143277a08df5244c4e745e936caf3
Aqib Ali <aliaqib13@gmail.com>
2024-07-25 17:19:14 +0500

    card compnent complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
-    borderRadius: {
-      '50p': '50%',
-      '25p': '25%',
-    },
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/common-component/**/*.{js,ts,jsx,tsx}"],
+  borderRadius: {
+    '50p': '50%',
+    '25p': '25%',
+  },
   theme: {
     extend: {},
   },
